8cd9a0bb9ccfeed608a7640b8e6297749a2daf31
alampobon34 <alampobon34@gmail.com>
2021-07-30 01:18:37 +0600

    staticfiles

--- food_ordering_project/urls.py
+++ food_ordering_project/urls.py
@@ -19,15 +19,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from accountApp.views import *
 from restaurantApp.views import *
+from django.views.static import serve
 
 urlpatterns = [
     path('',include('accountApp.urls')),
     path('',include('restaurantApp.urls')),
     path('',include('orderApp.urls')),
     path('admin/', admin.site.urls),
+    path(r'media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.STATICFILES_DIRS)
