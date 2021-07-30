b53f9a2e5f9ed717a17db399f871b017c723e571
alampobon34 <alampobon34@gmail.com>
2021-07-30 11:58:43 +0600

    collectstatic

--- staticfiles/js/cart.js
+++ staticfiles/js/cart.js
@@ -20,18 +20,33 @@ function updateUserOrder(item_id, action) {
 
     .then((data) => {
       console.log("data", data);
+      //var qty = document.getElementById("quantity").innerHTML=data;
       location.reload();
     });
 }
 
 for (var i = 0; i < updateBtns.length; i++) {
   var button = updateBtns[i];
+  
   button.addEventListener("click", function () {
     //console.log("button clicked")
     var item_id = this.dataset.fooditem;
     var action = this.dataset.action;
+    if(action=='delete'){
+      var con = confirm("Do you really want to delete this item?")
+      if(con==true){
+        action='delete'
+      }else{
+        action=''
+      }
+    }
     if (user == "AnonymousUser") {
       console.log("not logged in");
+      var con = confirm("Please login first..Do you want to redirect login page??")
+      if(con==true){
+        window.location.replace("http://127.0.0.1:8000/login/");
+          } 
+      
     } else {
       updateUserOrder(item_id, action);
     }
--- staticfiles/js/formValidation.js
+++ staticfiles/js/formValidation.js
@@ -39,6 +39,7 @@ function loginValidation() {
   }
 }
 
+
 function RegisterValidation() {
   clearError();
   var form = document.getElementById("registerForm");
@@ -185,3 +186,47 @@ function clearError() {
 })();
 
 
+
+function ShippingAddress(){
+  clearError();
+  var form = document.getElementById("shippingForm");
+
+  var address = document.forms["shippingForm"]["address"].value;
+  var area = document.forms["shippingForm"]["area"].value;
+  var houseNo = document.forms["shippingForm"]["houseNo"].value;
+  var zipcode = document.forms["shippingForm"]["zipcode"].value;
+
+
+  if (address == "") {
+    // alert("Enter a email address");
+    setError("add_address", "Enter the address...");
+    return false;
+  }
+  if (area == "") {
+    // alert("Enter a email address");
+    setError("add_area", "Enter the area...");
+    return false;
+  }
+  if (houseNo == "") {
+    // alert("Enter a email address");
+    setError("add_houseNo", "Enter the house no...");
+    return false;
+  }
+  if (zipcode == "") {
+    // alert("Enter a email address");
+    setError("add_zipcode", "enter the zipcode...");
+    return false;
+  }
+  if (zipcode == "." || zipcode=="e") {
+    // alert("Enter a email address");
+    setError("add_zipcode", "zip code must have digits...");
+    return false;
+  }
+  if (zipcode.length !=4) {
+    // alert("Enter a email address");
+    setError("add_zipcode", "Zip code has 4 digits...");
+    return false;
+  }
+
+
+}
