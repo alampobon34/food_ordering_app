66e537b3c4ff996cd53c188e0cbf3645e736cfcd
alampobon34 <alampobon34@gmail.com>
2021-08-21 04:39:16 +0600

    some static files uploaded

--- restaurantApp/tests.py
+++ restaurantApp/tests.py
@@ -4,7 +4,115 @@ from restaurantApp.models import *
 # Create your tests here.
 
 
-class TestModels(TestCase):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class RestaurantTest(TestCase):
 
     def setUp(self):
         self.restaurant1 = Restaurant.objects.create(
@@ -12,9 +120,45 @@ class TestModels(TestCase):
             address = "dhaka",
             description = "pasta pizza"
         )
+
+
     def test_restaurant(self):
-        print(self.restaurant1.name)
         self.assertEqual(self.restaurant1.name,"Street Oven")
 
+
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.restaurant1 = Restaurant.objects.create(
+            name = "Street Oven",
+            address = "dhaka",
+            description = "pasta pizza"
+        )
+
+        self.category1 = Category.objects.create(
+            restaurant = self.restaurant1,
+            name="Chicken")
+    
+    def test_category(self):
+        self.assertEqual(self.category1.name,"Chicken")
+
+
+class FoodItemTest(TestCase):
+    def setUp(self):
+        self.restaurant1 = Restaurant.objects.create(
+            name = "Street Oven",
+            address = "dhaka",
+            description = "pasta pizza"
+        )
+        self.category1 = Category.objects.create(
+            restaurant = self.restaurant1,
+            name = "Chicken"
+        )
+        self.fooditem1 = FoodItem.objects.create(
+            category = self.category1,
+            name = "pasta",
+            price= 300.00,
+        )
+    def test_fooditem(self):
+        self.assertEqual(self.fooditem1.name,"pasta")
 
 
