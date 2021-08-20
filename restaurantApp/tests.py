79d2ed0cea38cfc5995c3e1a764a0ba20b9d1857
KashSaif <2018-2-60-001@std.ewubd.edu>
2021-08-21 04:52:10 +0600

    restaurantapp models testinf done

--- restaurantApp/tests.py
+++ restaurantApp/tests.py
@@ -1,10 +1,7 @@
 from django.test import TestCase
 from restaurantApp.models import *
-
-# Create your tests here.
-
-
-class TestModels(TestCase):
+#TestingModel
+class RestaurantTest(TestCase):
 
     def setUp(self):
         self.restaurant1 = Restaurant.objects.create(
@@ -16,5 +13,38 @@ class TestModels(TestCase):
         print(self.restaurant1.name)
         self.assertEqual(self.restaurant1.name,"Street Oven")
 
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.restaurant1 = Restaurant.objects.create(
+            name = "Street Oven",
+            address = "dhaka",
+            description = "pasta pizza"
+        )
+        self.category1 = Category.objects.create(
+            restaurant = self.restaurant1,
+            name = "Chicken"
+        )
+    def test_category(self):
+        self.assertEqual(self.category1.name,"Chicken")
 
+class FoodItemTest(TestCase):
+    def setUp(self):
+        self.restaurant1 = Restaurant.objects.create(
+            name = "Street Oven",
+            address = "dhaka",
+            description = "pasta pizza"
+        )
+        self.category1 = Category.objects.create(
+            restaurant = self.restaurant1,
+            name = "Chicken"
+        )
+        self.fooditem1 = FoodItem.objects.create(
+            category = self.category1,
+            name = "pasta",
+            price = 525.52
+        )
+    def test_fooditem(self):
+        self.assertEqual(self.fooditem1.name,"pasta")
 
+#TestingURLS
+#         
