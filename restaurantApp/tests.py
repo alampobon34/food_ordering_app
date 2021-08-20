83a62151cd0f927e2f5969da94891b6174246276
alampobon34 <alampobon34@gmail.com>
2021-08-21 03:17:35 +0600

    static files changes

--- restaurantApp/tests.py
+++ restaurantApp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from restaurantApp.models import *
 
 # Create your tests here.
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.restaurant1 = Restaurant.objects.create(
+            name = "Street Oven",
+            address = "dhaka",
+            description = "pasta pizza"
+        )
+    def test_restaurant(self):
+        print(self.restaurant1.name)
+        self.assertEqual(self.restaurant1.name,"Street Oven")
+
+
+
